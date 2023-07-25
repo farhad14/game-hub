849a4a1b5e2c43f84da82d924b1c827eb2389cf6
= <=>
2023-07-25 18:30:50 +0330

    Build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -10,7 +10,7 @@ const ColorModeSwitch = () => {
         checked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
