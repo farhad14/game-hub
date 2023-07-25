2e0761ce57f8486b20f52e8052ccb075be80be46
= <=>
2023-07-25 23:24:57 +0330

    Refactor Game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,27 +13,26 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
   const skeletons = [0, 1, 2, 3, 4, 5, 6, 7];
 
+  if (error) return <Text color="red">{error}</Text>;
+
   return (
-    <div>
-      {error && <Text color="red">{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={6}
-        padding="10px"
-      >
-        {isLoading
-          ? skeletons.map((key) => (
-              <GameCardContainer key={key}>
-                <GameCardSkeleton />
-              </GameCardContainer>
-            ))
-          : games.map((game) => (
-              <GameCardContainer key={game.id}>
-                <GameCard game={game} />
-              </GameCardContainer>
-            ))}
-      </SimpleGrid>
-    </div>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={6}
+      padding="10px"
+    >
+      {isLoading
+        ? skeletons.map((key) => (
+            <GameCardContainer key={key}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))
+        : games.map((game) => (
+            <GameCardContainer key={game.id}>
+              <GameCard game={game} />
+            </GameCardContainer>
+          ))}
+    </SimpleGrid>
   );
 };
 
