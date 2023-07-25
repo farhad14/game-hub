3c32f65db4486d3013bb5623f37bac539fcb797b
= <=>
2023-07-25 08:41:20 +0330

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import getCroppedImageUrl from "../services/imageUrl";
 import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
 
@@ -9,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => (
   <Card borderRadius={10} overflow="hidden">
-    <Image src={game.background_image} />
+    <Image src={getCroppedImageUrl(game.background_image)} />
     <CardBody>
       <Heading fontSize="2xl">{game.name}</Heading>
       <HStack justifyContent="space-between">
