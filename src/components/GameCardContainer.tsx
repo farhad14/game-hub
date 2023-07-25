b69b8c9297af6945ffc8897857ed393c1d7b5424
= <=>
2023-07-25 12:45:32 +0330

    Display genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 const GameCardContainer = ({ children }: Props) => (
-  <Box borderRadius={10} overflow="hidden" width="300px">
+  <Box borderRadius={10} overflow="hidden">
     {children}
   </Box>
 );
